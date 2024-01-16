d4c73efc24326bd6311ebee9dba14d3521e411c8
nguyenhungthe2k3 <117711639+nguyenhungthe2k3@users.noreply.github.com>
2024-01-16 22:34:28 +0700

    manhinh welcome

---
+++ demo1/app/src/main/java/com/example/demo1/Login_Activity.java
@@ -0,0 +1,18 @@
+package com.example.demo1;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Login_Activity extends AppCompatActivity {
+
+}
